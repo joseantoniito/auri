6d9950a81803c389e8dd9f359df936636467163d
joseantoniito <jose.antonio.sandoval.flores@gmail.com>
2016-12-23 23:50:34 -0600

    Commit inicial pt. 1

---
+++ crm/application/logs/log-2016-12-23.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-12-23 19:24:28 --> Severity: Notice --> Trying to get property of non-object F:\xampp\htdocs\perfex_crm\crm\application\models\Misc_model.php 117
+ERROR - 2016-12-23 19:24:28 --> Severity: Notice --> Trying to get property of non-object F:\xampp\htdocs\perfex_crm\crm\application\models\Misc_model.php 117
+ERROR - 2016-12-23 19:29:48 --> Severity: Notice --> Trying to get property of non-object F:\xampp\htdocs\perfex_crm\crm\application\models\Misc_model.php 117
+ERROR - 2016-12-23 19:29:48 --> Severity: Notice --> Trying to get property of non-object F:\xampp\htdocs\perfex_crm\crm\application\models\Misc_model.php 117
+ERROR - 2016-12-23 19:30:18 --> Severity: Notice --> Trying to get property of non-object F:\xampp\htdocs\perfex_crm\crm\application\models\Misc_model.php 117
+ERROR - 2016-12-23 19:30:18 --> Severity: Notice --> Trying to get property of non-object F:\xampp\htdocs\perfex_crm\crm\application\models\Misc_model.php 117
+ERROR - 2016-12-23 19:30:19 --> Query error: Duplicate column name 'leadid' - Invalid query: ALTER TABLE `tblclients`
+	ADD `leadid` INT NULL DEFAULT NULL
+ERROR - 2016-12-23 19:30:41 --> Severity: Notice --> Trying to get property of non-object F:\xampp\htdocs\perfex_crm\crm\application\models\Misc_model.php 117
+ERROR - 2016-12-23 19:30:41 --> Severity: Notice --> Trying to get property of non-object F:\xampp\htdocs\perfex_crm\crm\application\models\Misc_model.php 117
+ERROR - 2016-12-23 19:31:01 --> Severity: Notice --> Trying to get property of non-object F:\xampp\htdocs\perfex_crm\crm\application\models\Misc_model.php 117
+ERROR - 2016-12-23 19:31:01 --> Severity: Notice --> Trying to get property of non-object F:\xampp\htdocs\perfex_crm\crm\application\models\Misc_model.php 117
+ERROR - 2016-12-23 19:31:01 --> Query error: Duplicate column name 'leadid' - Invalid query: ALTER TABLE `tblclients`
+	ADD `leadid` INT NULL DEFAULT NULL
---
+++ crm/application/logs/log-2016-12-24.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-12-24 02:24:38 --> 404 Page Not Found: admin/Login/index
+ERROR - 2016-12-24 02:27:56 --> 404 Page Not Found: admin/Login/index
+ERROR - 2016-12-24 02:37:52 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'auri_perfex'@'localhost' (using password: YES) F:\xampp\htdocs\perfex_crm\crm\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2016-12-24 02:37:52 --> Unable to connect to the database
+ERROR - 2016-12-24 02:41:28 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'auri_perfex'@'localhost' (using password: YES) F:\xampp\htdocs\perfex_crm\crm\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2016-12-24 02:41:28 --> Unable to connect to the database
+ERROR - 2016-12-24 02:41:59 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'auri_perfex'@'localhost' (using password: YES) F:\xampp\htdocs\perfex_crm\crm\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2016-12-24 02:41:59 --> Unable to connect to the database
+ERROR - 2016-12-24 02:44:23 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'auri_perfex'@'localhost' (using password: YES) F:\xampp\htdocs\perfex_crm\crm\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2016-12-24 02:44:23 --> Unable to connect to the database
+ERROR - 2016-12-24 02:45:11 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'auri_perfex'@'localhost' (using password: YES) F:\xampp\htdocs\perfex_crm\crm\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2016-12-24 02:45:11 --> Unable to connect to the database
